2c360c3435e7826111e1665058856904f664b93c
Jasurbek <jasurisoqov685@gmail.com>
2022-05-08 19:25:56 +0500

    form useRef current reset

--- src/Components/todos-form.js
+++ src/Components/todos-form.js
@@ -1,16 +1,34 @@
 import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
+import addTodo from "../actions/todos";
+
 const TodosForm = () => {
   const inputRef = useRef(null);
   const dispatch = useDispatch();
+  const formRef = useRef(null);
+
+  const returnTiem = () => {
+    let date = new Date().getTime();
+    return date;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(inputRef.current.value);
+
+    dispatch(
+      addTodo({
+        text: inputRef.current.value,
+        id: returnTiem(),
+        isDone: false,
+      })
+    );
+
+    formRef.current.reset();
   };
 
   return (
-    <form className="my_form" onSubmit={handleSubmit}>
+    <form className="my_form" onSubmit={handleSubmit} ref={formRef}>
       <input className="write_data" ref={inputRef} />
       <button className="btn_add btn">Add</button>
     </form>
