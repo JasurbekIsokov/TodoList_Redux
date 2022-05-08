9311de03e7158bdc7d756f5b54c66d11a49baee4
Jasurbek <jasurisoqov685@gmail.com>
2022-05-08 18:59:06 +0500

    import useRef and create handleSubmit function

--- src/Components/todos-form.js
+++ src/Components/todos-form.js
@@ -1,7 +1,16 @@
+import { useRef } from "react";
+
 const TodosForm = () => {
+  const inputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+  };
+
   return (
-    <form className="my_form">
-      <input className="write_data" />
+    <form className="my_form" onSubmit={handleSubmit}>
+      <input className="write_data" ref={inputRef} />
       <button className="btn_add btn">Add</button>
     </form>
   );
