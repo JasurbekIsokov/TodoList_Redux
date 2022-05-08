d3c1cb96f490a1ec1cf61e6e283e128a41ddd68b
Jasurbek <jasurisoqov685@gmail.com>
2022-05-08 20:30:06 +0500

    remove todo finished

--- src/Components/todo-item.js
+++ src/Components/todo-item.js
@@ -1,11 +1,23 @@
+import { Dispatch } from "react";
+import { useDispatch } from "react-redux";
+
+import { removeTodo } from "../actions/todos";
+
 const TodoItem = ({ text, id }) => {
+  const dispatch = useDispatch();
+
   return (
     <div className="card">
       <div className="card_left">
         <input className="check_btn btn" type={"checkbox"} />
         <label>{text}</label>
       </div>
-      <button className="delete_btn btn">Delete</button>
+      <button
+        className="delete_btn btn"
+        onClick={() => dispatch(removeTodo(id))}
+      >
+        Delete
+      </button>
     </div>
   );
 };
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -7,6 +7,8 @@ const todos = (state = initialState, action) => {
     case TODO.ADD_TODO:
       return [...state, action.payload];
 
+    case TODO.REMOVE_TODO:
+      return state.filter((item) => item.id !== action.payload.id);
     default:
       return state;
   }
